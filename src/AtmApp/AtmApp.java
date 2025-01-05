ff56dbefdeb4ea1971a550e47ca590650ff1f1c4
Chris Ftoulis <christosftoulis3@gmail.com>
2025-01-05 10:55:32 +0200

    Atm app update

--- src/AtmApp/AtmApp.java
+++ src/AtmApp/AtmApp.java
@@ -2,16 +2,12 @@ package AtmApp;
 
 import java.util.Scanner;
 
-/**
- * Creation of a simple Atm program
- * with menu and the usual transactions
- */
-
 public class AtmApp {
 
-    static double account = 1500;
-    static double nAccount;
-    static double newAccount;
+    static double primaryAccount = 1500;
+    static double backupAccount = 1000;
+    static double newBackupAccount;
+    static double transferWithdraw = 0;
     static double deposit = 0;
     static double withdraw = 0;
     static double dAccount;
@@ -19,50 +15,62 @@ public class AtmApp {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println(atmMenu());
+
+        atmAppMenu();
 
     }
 
-    public static double atmMenu() {
+        public static void atmAppMenu() {
+            while (true) {
+                System.out.println("Atm app menu: ");
+                System.out.println("1. Make a deposit");
+                System.out.println("2. Make a withdraw");
+                System.out.println("3. Account balance");
+                System.out.println("4. account transfer");
+                System.out.println("5. Exit Atm App menu");
 
-        int inputChoice;
+                int inputChoice = scanner.nextInt();
 
-        do {
-            System.out.println("Please make a selection: ");
-            System.out.println("1. Make a deposit");
-            System.out.println("2. Make a withdraw");
-            System.out.println("3. Check account balance");
-            System.out.println("4. Make accounts transfers");
-            System.out.println("5. Exit");
-            inputChoice = scanner.nextInt();
+                switch (inputChoice) {
+                    case 1:
+                        System.out.println("Select the deposit amount");
+                        deposit = scanner.nextDouble();
+                        break;
 
-            if (inputChoice == 1) {
-                System.out.println("Deposit. Please insert an amount: ");
-                deposit = scanner.nextDouble();
-                dAccount = account + deposit;
-                System.out.println("Your new balance after the deposit is: " + dAccount);
-                break;
-            } else if (inputChoice == 2) {
-                System.out.println("Withdraw. Please insert an amount: ");
-                withdraw = scanner.nextDouble();
-                wAccount = account - withdraw;
-                System.out.println("Your new balance after the withdraw is: " + wAccount);
-                break;
-            }
-            else if (inputChoice == 3) {
-                if ((dAccount >= account)) {
-                    nAccount = dAccount;
-                    System.out.println("Your account balance is: " + nAccount);
-                    break;
-                } else if ((wAccount != account)) {
-                    newAccount = wAccount;
-                    System.out.println("Your account balance is: " + newAccount);
-                    break;
+                    case 2:
+                        System.out.println("Select the withdraw amount");
+                        withdraw = scanner.nextDouble();
+                        break;
+
+                    case 3:
+                        System.out.println("Check account balance");
+                        if (deposit > 0) {
+                            dAccount = primaryAccount + deposit;
+                            System.out.println("Your account balance is: " + dAccount);
+                        } else if (withdraw > 0) {
+                            wAccount = primaryAccount - withdraw;
+                            System.out.println("Your account balance is: " + wAccount);
+
+                        } break;
+
+                    case 4:
+                        System.out.println("Account transfer");
+                        transferWithdraw = scanner.nextDouble();
+                        newBackupAccount = backupAccount + transferWithdraw;
+                        System.out.println("Your backup account balance was: " + backupAccount +"\n" +
+                                "Your new balance is: " + newBackupAccount);
+                        break;
+
+                    case 5:
+                        System.out.println("Exit menu.");
+                        scanner.close();
+                        return;
+
+                    default:
+                        System.out.println("Wrong entry. \n" +
+                                "Please make a selection.");
+
                 }
             }
-
-        } while (inputChoice != 5);
-            return atmMenu();
-
+        }
     }
-}
