954267b304ee871bd109695c7a7aa36da5c978eb
Chris Ftoulis <christosftoulis3@gmail.com>
2025-01-07 15:56:28 +0200

    Atm App update (create methods)

--- src/AtmApp/AtmApp.java
+++ src/AtmApp/AtmApp.java
@@ -45,83 +45,20 @@ public class AtmApp {
 
                 switch (inputChoice) {
                     case 1:
-                        System.out.println("Select the deposit amount");
-                        try (PrintStream pDeposit = new PrintStream(new PrintStream("c:/AtmApp/deposit.txt"))) {
-                            System.out.println("Your transaction will be printed in file");
-                            deposit = scanner.nextDouble();
-                            if (deposit > 0) {
-                                primaryAccount += deposit;
-                                System.out.println("You successfully deposit: " + deposit);
-                                System.out.println("Your new account balance is: " + primaryAccount);
-                                pDeposit.println("ATM APP PRINT ACCOUNT \n" +
-                                        "You successfully deposit: " + deposit + "\n" +
-                                        "Your new account balance is: " + primaryAccount);
-                            } else {
-                                System.out.println("The amount of deposit is invalid");
-                            }
-                        } catch (FileNotFoundException e) {
-                            System.err.println("The file is not found or cannot be created.");
-                        }
+                        depositCase();
                         break;
 
                     case 2:
-                        System.out.println("Select the withdraw amount");
-                        try (PrintStream pWithdraw = new PrintStream(new PrintStream("c:/AtmApp/withdraw.txt"))) {
-                            System.out.println("Your transaction will be printed in file");
-                            withdraw = scanner.nextDouble();
-                            if (withdraw > 0 && withdraw <= primaryAccount) {
-                                primaryAccount -= withdraw;
-                                System.out.println("You successfully withdraw: " + withdraw);
-                                System.out.println("Your new account balance is: " + primaryAccount);
-                                pWithdraw.println("ATM APP PRINT ACCOUNT \n" +
-                                        "You successfully withdraw: " + withdraw + "\n" +
-                                        "Your new account balance is: " + primaryAccount);
-                            } else {
-                                System.out.println("The withdraw is invalid or there are insufficient funds.");
-                            }
-                        } catch (FileNotFoundException e) {
-                            System.err.println("The file is not found or cannot be created.");
-                        } break;
+                        withdrawCase();
+                        break;
 
-                            case 3:
-                                try (PrintStream pAccount = new PrintStream(new PrintStream("c:/AtmApp/account-balance.txt"))) {
-                                    System.out.println("Your transaction will be printed in file");
-                                    System.out.println("Your account balance is: " + primaryAccount);
-                                    pAccount.println("ATM APP PRINT ACCOUNT \n" +
-                                            "Your accounts balance are: \n" +
-                                            "Primary account: " + primaryAccount + "\n" +
-                                            "Backup account: " + backupAccount);
+                    case 3:
+                        accountsBalance();
+                        break;
 
-                                } catch (FileNotFoundException e) {
-                                    System.err.println("The file is not found or cannot be created.");
-                                }
-                                break;
-
-                            case 4:
-                                System.out.println("Account transfer");
-                                try (PrintStream pTransfer = new PrintStream(new PrintStream("c:/AtmApp/account-transfer.txt"))) {
-                                    System.out.println("Your transaction will be printed in file \n" +
-                                            "Please enter the transfer amount");
-                                    transferWithdraw = scanner.nextDouble();
-                                    pTransfer.println("ATM APP PRINT ACCOUNT \n" +
-                                            "The primary account balance : " + primaryAccount + "\n" +
-                                            "The backup account balance : " + backupAccount);
-                                    if (transferWithdraw > 0 && transferWithdraw <= primaryAccount) {
-                                        primaryAccount -= transferWithdraw;
-                                        backupAccount += transferWithdraw;
-                                        System.out.println("You successfully transfer the amount: " + transferWithdraw + "\n" +
-                                                "Your new primary account balance is: " + primaryAccount + "\n" +
-                                                "Your new backup account balance is: " + backupAccount);
-                                        pTransfer.println("Transfer amount : " + transferWithdraw);
-                                        pTransfer.println("The primary account balance : " + primaryAccount + "\n" +
-                                                "The backup account balance : " + backupAccount);
-                                    } else {
-                                        System.out.println("The transfer you are trying is invalid or there are insufficient funds!");
-                                    }
-                                } catch (FileNotFoundException e) {
-                                    System.err.println("The file is not found or cannot be created.");
-                                }
-                                break;
+                    case 4:
+                        accountsTransfer();
+                        break;
 
                             case 5:
                                 System.out.println("Exit menu.");
@@ -133,6 +70,88 @@ public class AtmApp {
                                         "Please make a selection.");
                         }
 
+                }
+            }
+
+            public static void depositCase() {
+                System.out.println("Select the deposit amount");
+                try (PrintStream pDeposit = new PrintStream(new PrintStream("c:/AtmApp/deposit.txt"))) {
+                    System.out.println("Your transaction will be printed in file");
+                    deposit = scanner.nextDouble();
+                    if (deposit > 0) {
+                        primaryAccount += deposit;
+                        System.out.println("You successfully deposit: " + deposit);
+                        System.out.println("Your new account balance is: " + primaryAccount);
+                        pDeposit.println("ATM APP PRINT ACCOUNT \n" +
+                                "You successfully deposit: " + deposit + "\n" +
+                                "Your new account balance is: " + primaryAccount);
+                    } else {
+                        System.err.println("The amount of deposit is invalid");
+                    }
+                } catch (FileNotFoundException e) {
+                    System.err.println("The file is not found or cannot be created.");
+                }
+            }
+
+            public static void withdrawCase() {
+                System.out.println("Select the withdraw amount");
+                try (PrintStream pWithdraw = new PrintStream(new PrintStream("c:/AtmApp/withdraw.txt"))) {
+                    System.out.println("Your transaction will be printed in file");
+                    withdraw = scanner.nextDouble();
+                    if (withdraw > 0 && withdraw <= primaryAccount) {
+                        primaryAccount -= withdraw;
+                        System.out.println("You successfully withdraw: " + withdraw);
+                        System.out.println("Your new account balance is: " + primaryAccount);
+                        pWithdraw.println("ATM APP PRINT ACCOUNT \n" +
+                                "You successfully withdraw: " + withdraw + "\n" +
+                                "Your new account balance is: " + primaryAccount);
+                    } else {
+                        System.err.println("The withdraw is invalid or there are insufficient funds.");
+                    }
+                } catch (FileNotFoundException e) {
+                    System.err.println("The file is not found or cannot be created.");
+                }
+            }
+
+            public static void accountsBalance() {
+                try (PrintStream pAccount = new PrintStream(new PrintStream("c:/AtmApp/account-balance.txt"))) {
+                    System.out.println("Your transaction will be printed in file");
+                    System.out.println();
+                    System.out.println("Your primary account balance is: " + primaryAccount);
+                    System.out.println("Your backup account balance is: " + backupAccount);
+                    pAccount.println("ATM APP PRINT ACCOUNT \n" +
+                            "Your accounts balance are: \n" +
+                            "Primary account: " + primaryAccount + "\n" +
+                            "Backup account: " + backupAccount);
+
+                } catch (FileNotFoundException e) {
+                    System.err.println("The file is not found or cannot be created.");
+                }
+            }
+
+            public static void accountsTransfer() {
+                System.out.println("Account transfer");
+                try (PrintStream pTransfer = new PrintStream(new PrintStream("c:/AtmApp/account-transfer.txt"))) {
+                    System.out.println("Your transaction will be printed in file \n" +
+                            "Please enter the transfer amount");
+                    transferWithdraw = scanner.nextDouble();
+                    pTransfer.println("ATM APP PRINT ACCOUNT \n" +
+                            "The primary account balance : " + primaryAccount + "\n" +
+                            "The backup account balance : " + backupAccount);
+                    if (transferWithdraw > 0 && transferWithdraw <= primaryAccount) {
+                        primaryAccount -= transferWithdraw;
+                        backupAccount += transferWithdraw;
+                        System.out.println("You successfully transfer the amount: " + transferWithdraw + "\n" +
+                                "Your new primary account balance is: " + primaryAccount + "\n" +
+                                "Your new backup account balance is: " + backupAccount);
+                        pTransfer.println("Transfer amount : " + transferWithdraw);
+                        pTransfer.println("The primary account balance : " + primaryAccount + "\n" +
+                                "The backup account balance : " + backupAccount);
+                    } else {
+                        System.err.println("The transfer you are trying is invalid or there are insufficient funds!");
+                    }
+                } catch (FileNotFoundException e) {
+                    System.err.println("The file is not found or cannot be created.");
                 }
             }
         }
